982e06705a487d5fb2477e95e785c9bb2b73f3a9
Forrest Waters <forrestwaters@gmail.com>
2020-04-21 21:24:23 +0800

    simple frontend for first backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [newName, setNewName] = React.useState("");
+  const [allNames, setAllName] = React.useState([]);
+
+  const getLeaderboard = () =>
+    axios.get("http://localhost:3000/leaderboard").then((res) => {
+      const dbNames = res.data;
+      setAllName(dbNames);
+      return null;
+    });
+
+  React.useEffect(() => {
+    const unsubscribe = getLeaderboard();
+    return () => unsubscribe();
+  }, []);
+  const handleSubmit = () => {
+    axios.post("http://localhost:3000/players", { name: newName }).then(() => {
+      setNewName("");
+      return getLeaderboard();
+    });
+  };
+  const handleDelete = (playerId) => () => {
+    console.log("playerId", playerId);
+    axios.delete(`http://localhost:3000/players/${playerId}`).then((res) => {
+      console.log(res);
+      return getLeaderboard();
+    });
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Simple Registration</h2>
+        <p>Add name to database and display all stats</p>
+        <br />
+        <form>
+          <input
+            placeholder="Name"
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+          />
+          <button onClick={handleSubmit}>Submit</button>
+        </form>
+        <br />
+        <table>
+          <thead>
+            <tr>
+              <td>Name</td>
+              <td>Stats</td>
+            </tr>
+          </thead>
+          <tbody>
+            {allNames.map((x, index) => (
+              <tr key={index}>
+                <td>{x.name}</td>
+                <td>{x.stats}</td>
+                <td>
+                  <button onClick={handleDelete(x.playerId)}>Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
